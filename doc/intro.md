ef9baa396e7b228e8e41d9646e32874929b4cdad
Ludwig <lk093@hdm-stuttgart.de>
2017-12-28 13:06:48 +0100

    app project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clotalk
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/clotalk/core.clj
@@ -0,0 +1,7 @@
+(ns clotalk.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
