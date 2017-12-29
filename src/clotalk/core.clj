2dee31a9d9dae85c98176fcca11bd2c7116bdcd9
Ludwig <lk093@hdm-stuttgart.de>
2017-12-29 13:25:49 +0100

    added namespace and tested features

--- src/clotalk/core.clj
+++ src/clotalk/core.clj
@@ -1,7 +1,12 @@
 (ns clotalk.core
-  (:gen-class))
+  (:gen-class)
+  (:require [ns-test.test :refer :all]))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(test-fn 3)
+(println vector1)
---
+++ src/ns-test/test.clj
@@ -0,0 +1,7 @@
+(ns ns-test.test)
+
+(defn test-fn
+  [x]
+  (print (+ x 2)))
+
+(def vector1 [2 3])
