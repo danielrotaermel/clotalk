b47fbf6f5711062bd8db9cbc4b1d726dd0055548
Ludwig <lk093@hdm-stuttgart.de>
2018-01-10 11:30:42 +0100

    test

--- src/clotalk/server.clj
+++ src/clotalk/server.clj
@@ -2,10 +2,13 @@
   (:use ring.adapter.jetty))
 
 (defn handler [request]
-    {:status 200
-     :headers {"Content-Type" "text/html"}
-     :body "Hello World"})
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "<h1>Hello World</h1>"})
+
 
 ;; start REPL
 ;; in repl => (.start sever) / (.stop server) and <shift + ENTER>
+;; stop process on port:
+;; lsof -P | grep ':3000' | awk '{print $2}' | xargs kill -9)
 (defonce server (run-jetty #'handler {:port 3000 :join? false}))
