06d0020a943fc5fa7cbf73ee5f1e620f51b51589
Daniel Rotärmel <ezekills@gmx.de>
2018-01-10 14:35:50 +0100

    added hot reload server

--- project.clj
+++ project.clj
@@ -6,7 +6,10 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [proto-repl "0.3.1"]
                  [ring/ring-core "1.6.3"]
-                 [ring/ring-jetty-adapter "1.6.3"]]
+                 [ring/ring-jetty-adapter "1.6.3"]
+                 [ring/ring-devel "1.6.3"]]
   :main ^:skip-aot clotalk.core
   :target-path "target/%s"
+  :ring {:handler clotalk.server/handler}
+  :plugins [[lein-ring "0.12.1"]]
   :profiles {:uberjar {:aot :all}})
--- src/clotalk/server.clj
+++ src/clotalk/server.clj
@@ -1,14 +1,25 @@
 (ns clotalk.server
   (:use ring.adapter.jetty))
 
+
+  ;; Now, let's look at the information that is going in and out of our application
+(require 'clojure.pprint)
+
+  ;; First we'll delegate the actual functionality of our app to a handler
+
 (defn handler [request]
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body "<h1>Hello World</h1>"})
 
+  ;; And then we'll wrap that in a wrapper that prints the incoming and outgoing data:
 
-;; start REPL
-;; in repl => (.start sever) / (.stop server) and <shift + ENTER>
-;; stop process on port:
-;; lsof -P | grep ':3000' | awk '{print $2}' | xargs kill -9)
-(defonce server (run-jetty #'handler {:port 3000 :join? false}))
+(defn app [request]
+  (println "-------------------------------")
+  (println "Incoming Request:")
+  (clojure.pprint/pprint request)
+  (let [response (handler request)]
+    (println "Outgoing Response Map:")
+    (clojure.pprint/pprint response)
+    (println "-------------------------------")
+    response))
